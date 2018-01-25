b403797d2b345d7f64dc0b06e7cfd4c63452c6e3
raphsutti <neoster9@gmail.com>
2018-01-25 22:25:35 +1100

    mouseenter function to show description

--- menu/javascript.js
+++ menu/javascript.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	// alert('hi');
+	$('div').mouseenter(function() {
+		$(this).toggleClass('show-description');
+	});
 	$('div').click(function() {
 		$(this).toggleClass('show-description');
 	});
