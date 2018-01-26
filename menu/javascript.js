5d48fe577181190694c808105f2f80efa16611f0
raphsutti <neoster9@gmail.com>
2018-01-26 16:11:20 +1100

    nav bar with active label

--- menu/javascript.js
+++ menu/javascript.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
 	$('div').mouseenter(function() {
 		$(this).toggleClass('show-description');
 	});
-	$('div').click(function() {
-		$(this).toggleClass('show-description');
+	// $('div').click(function() {
+	// 	$(this).toggleClass('show-description');
+	// });
+
+	$(".nav-link").on("click", function(){
+	   $(".nav-item").removeClass("active");
+	   $(this).parent().addClass("active");
 	});
 })
