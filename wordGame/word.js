677b76eea43876d7ff0c04d98eede4dd2e7eb817
Raph <rsuttiyotin@seek.com.au>
2020-03-04 08:06:02 +1100

    Allow input of the question into individual squares

---
+++ wordGame/word.js
@@ -0,0 +1,9 @@
+var squares = document.querySelectorAll(".square");
+
+const selectElement = document.querySelector("#input");
+selectElement.addEventListener("change", event => {
+  resultArray = event.target.value.split("");
+  for (var i = 0; i < resultArray.length; i++) {
+    squares[i].textContent = event.target.value[i];
+  }
+});
