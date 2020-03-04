4d1df019a8fcf888ad72fe3a65bd7e317322e8ed
Raph <rsuttiyotin@seek.com.au>
2020-03-04 17:19:14 +1100

    Limit input length to 9

--- wordGame/word.js
+++ wordGame/word.js
@@ -1,7 +1,7 @@
 const squares = document.querySelectorAll(".square");
 
-const selectElement = document.querySelector("#input");
-selectElement.addEventListener("change", event => {
+const inputElement = document.querySelector("#input");
+inputElement.addEventListener("change", event => {
   resultArray = event.target.value.split("");
   for (var i = 0; i < resultArray.length; i++) {
     squares[i].textContent = event.target.value[i];
