8a87ee6330be55a0ea9c8a90899770a2dac2688f
Raph <rsuttiyotin@seek.com.au>
2020-07-30 13:55:34 +1000

    Initial commit

---
+++ cs253/local-listen.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { exec } = require('child_process')
+
+const COMMAND = 'open /Applications/Dictionary.app'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  exec(COMMAND, err => {
+    if (err) res.status(500).send(err.stack)
+    else res.status(200).send('Success')
+  })
+})
+
+app.listen(4000, '127.0.0.1')
