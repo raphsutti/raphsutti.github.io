0aa69b9fc2d1f1d9559c768c95bc01e2bda5dfec
Raph <rsuttiyotin@seek.com.au>
2020-08-13 15:21:02 +1000

    Initial commit

--- cs253/local-listen.js
+++ cs253/local-listen.js
@@ -5,12 +5,18 @@ const COMMAND = 'open /Applications/Dictionary.app'
 
 const app = express()
 
+app.options('/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', 'http://attacker.com:9999')
+  res.set('Access-Control-Allow-Methods', 'PUT')
+  res.send('ok')
+})
+
 app.get('/', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
   exec(COMMAND, err => {
-    // res.set('Access-Control-Allow-Origin', '*')
     if (err) res.status(500).send(err.stack)
     else res.status(200).send('Success')
   })
 })
 
-app.listen(4000, '127.0.0.1')
+app.listen(8080, '127.0.0.1')
